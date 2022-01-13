0b0bc76b87abafd4b3090a1716d2cdde43467862
Alex <alex.wood91@outlook.com>
2022-01-13 16:54:23 +0000

    skinny controllers

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,22 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(alex, jamie) }
   let(:alex) { double :player }
   let(:jamie) { double :player }
+
+  describe "#player1" do
+    it "it returns player 1" do
+      expect(game.player1).to eq alex
+    end
+  end
+
+  describe "#player2" do
+    it "it returns player 2" do
+      expect(game.player2).to eq jamie
+    end
+  end
+
 
   describe '#attack' do
     it 'damages the player' do
